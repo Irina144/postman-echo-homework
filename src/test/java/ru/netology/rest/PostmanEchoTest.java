7f9df62fa35b2fb37bda4a7e8ac9f44f6c558eee
Irina144 <solnechnayamichelle@gmail.com>
2025-10-20 20:13:46 +0300

    Добавлен тест Postmat Echo

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,19 +1,14 @@
-package ru.netology.rest;
-
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
 public class PostmanEchoTest {
-
     @Test
     void shouldReturnSentTextInDataField() {
-        // что отправляем:
         String payload = "some data";
 
-        // сам тест
+        useRelaxedHTTPSValidation(); 
+
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
@@ -22,7 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-
-                .body("dataX", equalTo(payload));
+                .body("data", equalTo(payload));
     }
 }
