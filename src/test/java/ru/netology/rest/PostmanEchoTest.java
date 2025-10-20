55b2a70bcde886f0db8fa81ff52783891ddeef96
Irina144 <solnechnayamichelle@gmail.com>
2025-10-20 19:48:50 +0300

    Init: gradle project + rest-Assured postman acho test + CI

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology.rest;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentTextInDataField() {
+        // что отправляем:
+        String payload = "some data";
+
+        // сам тест
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(payload)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+
+                .body("data", equalTo(payload));
+    }
+}
