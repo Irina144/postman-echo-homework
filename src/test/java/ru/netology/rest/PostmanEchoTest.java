6d1f860200ad9ddb1b33df6b61d3fac415f7d779
Irina144 <solnechnayamichelle@gmail.com>
2025-10-20 20:38:01 +0300

    test: fail on wrong JSONath (dataX)

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(payload));
+                .body("dataX", equalTo(payload));
     }
 }
